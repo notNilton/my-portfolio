032fc1686624eadfd86bb30f7bee84f0e81b04e2
Nilton Santos <nilton.naab@gmail.com>
2025-02-17 16:55:39 -0400

    chore: minor changes

--- webapp-client/src/data/AboutData.ts
+++ webapp-client/src/data/AboutData.ts
@@ -4,7 +4,7 @@ export interface About {
 }
 
 export const aboutData: About = {
-  title: "Quem sou eu?",
+  title: "Sobre Mim",
   description:
     "Sou um desenvolvedor apaixonado por tecnologia e inovação, com experiência em desenvolvimento backend e criação de APIs eficientes. Sempre em busca de novos desafios para expandir meus conhecimentos e contribuir para projetos impactantes.",
 };
