d618e8b9c6ff9eab741535debe789a899f5c46d3
Nilton Santos <nilton.naab@gmail.com>
2025-02-24 18:05:53 -0400

    style: minor page changes

--- src/components/CurriculumComp.tsx
+++ src/components/CurriculumComp.tsx
@@ -3,25 +3,41 @@ import { curriculumData } from "../data/CurriculumData";
 import "../styles/CurriculumStyle.css";
 
 const Curriculum: FC = () => {
+  const groupedData = {
+    education: curriculumData.filter((item) => item.type === "education"),
+    projects: curriculumData.filter((item) => item.type === "projects"),
+    work: curriculumData.filter((item) => item.type === "work"),
+  };
+
   return (
     <section className="curriculum">
-      <h2>Meu Currículo</h2>
-      <div className="curriculum-list">
-        {curriculumData.map((phase) => (
-          <div key={phase.id} className="curriculum-card">
-            <div className="curriculum-info">
-              <h3 className="curriculum-title">{phase.title}</h3>
-              <p className="curriculum-date">{phase.date}</p>
-              <p className="curriculum-short-description">
-                {phase.shortDescription}
-              </p>
-              <p className="curriculum-long-description">
-                {phase.longDescription}
-              </p>
-            </div>
+      {Object.entries(groupedData).map(([key, phases]) => (
+        <div key={key} className="curriculum-section">
+          <h3 className="curriculum-section-title">
+            {key === "education"
+              ? "Educação"
+              : key === "projects"
+              ? "Projetos"
+              : "Experiência Profissional"}
+          </h3>
+          <div className="curriculum-list">
+            {phases.map((phase) => (
+              <div key={phase.id} className="curriculum-card">
+                <div className="curriculum-info">
+                  <h3 className="curriculum-title">{phase.title}</h3>
+                  <p className="curriculum-date">{phase.date}</p>
+                  <p className="curriculum-short-description">
+                    {phase.shortDescription}
+                  </p>
+                  <p className="curriculum-long-description">
+                    {phase.longDescription}
+                  </p>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </section>
   );
 };
--- src/components/ProjectsComp.tsx
+++ src/components/ProjectsComp.tsx
@@ -5,7 +5,6 @@ import '../styles/ProjectsStyle.css';
 const Projects: FC = () => {
   return (
     <section className="projects">
-      <h2>Projetos</h2>
       <div className="project-list">
         {projectList.map(project => (
           <div key={project.id} className="project-card">
