f88d3e868c6f7c14163b08447e6eb765e8678c1d
Nilton Santos <nilton.naab@gmail.com>
2025-02-25 10:43:55 -0400

    feat: add real project data

--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -1,10 +1,13 @@
-import radareImage from '../assets/projects/projectB.webp';
-import aiicpImage from '../assets/projects/projectB.webp';
+import radareImage1 from '../assets/projects/interface-completa.png';
+import aiicpImage1 from '../assets/projects/comparison.png';
+import aiicpImage2 from '../assets/projects/aiicp1.png';
+// import aiicpImage3 from '../assets/projects/aiicp2.png';
+// import aiicpImage4 from '../assets/projects/aiicp3.png';
 
 export interface Project {
   id: number;
   name: string;
-  image: string;
+  images: string[];
   description: string;
   technologies: string[];
 }
@@ -13,15 +16,15 @@ export const projectList: Project[] = [
   {
     id: 1,
     name: 'RADARE - Reconciliation and Data Analysis in a Responsive Environment',
-    image: radareImage,
-    description: 'Sistema Sistema de reconciliação de dados industriais, garantindo integridade e eficiência na análise de grandes volumes de dados.de reconciliação de dados industriais, garantindo integridade e eficiência na análise de grandes volumes de dados.',
-    technologies: ['Python', 'TypeScript', 'SQLite']
+    images: [radareImage1],
+    description: 'O RADARE é um sistema avançado de reconciliação de dados industriais, projetado para processar e validar grandes volumes de informações em tempo real. Ele suporta milhares de reconciliações por minuto, garantindo a integridade e consistência dos dados processados. Com algoritmos robustos de correção, o sistema elimina 100% das inconsistências antes da consolidação final. Sua arquitetura responsiva e altamente escalável permite integração com múltiplas fontes de dados, proporcionando um desempenho otimizado para ambientes industriais críticos.',
+    technologies: ['Python', 'TypeScript', 'FastAPI', 'PostgreSQL', 'Redis', 'Docker', 'Kubernetes']
   },
   {
     id: 2,
     name: 'AIICP - Artificial Intelligence Image Correction and Augmentation Pipeline',
-    image: aiicpImage,
-    description: 'Pipeline automatizado para correção e aprimoramento de imagens pixel-art geradas por Inteligência Artificial.Sistema de reconciliação de dados industriais, garantindo integridade e eficiência na análise de grandes volumes de dados.',
-    technologies: ['Python', 'Flask', 'OpenCV', 'PIL']
+    images: [aiicpImage1, aiicpImage2],
+    description: 'O AIICP é um pipeline automatizado de inteligência artificial para tratamento, correção e aprimoramento de imagens, especialmente em pixel-art geradas por IA. Ele opera através de um processo em múltiplas etapas, iniciando com pré-processamento e limpeza da imagem, seguido por algoritmos avançados de ajuste de cores, upscaling e reconstrução de detalhes. Utilizando técnicas de Deep Learning, redes neurais convolucionais (CNN) e métodos de aprimoramento com OpenCV, o sistema entrega imagens de alta qualidade, preservando a fidelidade ao estilo original.',
+    technologies: ['Python', 'Flask', 'TensorFlow', 'OpenCV', 'PIL', 'NumPy', 'FastAPI', 'PyTorch']
   }
 ];
