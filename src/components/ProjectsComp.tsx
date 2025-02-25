48f6026a7fda925a60ffe9346a5192f56f617e12
Nilton Santos <nilton.naab@gmail.com>
2025-02-25 10:51:54 -0400

    chore: minor changes to adapt to the new image carousel component

--- src/components/ProjectsComp.tsx
+++ src/components/ProjectsComp.tsx
@@ -1,18 +1,15 @@
-import { FC } from 'react';
-import { projectList } from '../data/ProjectsData';
-import '../styles/ProjectsStyle.css';
+import { FC } from "react";
+import { projectList } from "../data/ProjectsData";
+import ImageCarousel from "./ImageCarousel"; // Importando o componente do carrossel
+import "../styles/ProjectsStyle.css";
 
 const Projects: FC = () => {
   return (
     <section className="projects">
       <div className="project-list">
-        {projectList.map(project => (
+        {projectList.map((project) => (
           <div key={project.id} className="project-card">
-            <img
-              src={project.image}
-              alt={project.name}
-              className="project-image"
-            />
+            <ImageCarousel images={project.images} /> {/* Agora recebe um array diretamente */}
             <div className="project-info">
               <h3 className="project-title">{project.name}</h3>
               <p className="project-description">{project.description}</p>
